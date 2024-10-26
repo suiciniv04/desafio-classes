139f6763f86596eb2e606bf56b075dbf9e142cab
viniciusmo <viniciusmotaes@gmail.com>
2024-10-26 18:52:45 -0300

    Desafio do Felipão da DIO

--- desafio.js
+++ desafio.js
@@ -1,2 +1,32 @@
-console.log("Olá mundão")
-console.log("Teste de atualização git")
+
+let nome = "PODEROSO NINJA" 
+let XP = 10000     
+let nvl
+
+if (XP < 1000) {
+    nvl = "Ferro"
+
+} else if (XP >= 1001 && xp <= 2000) {
+    nvl = "Bronze"
+
+} else if (XP >= 2001 && xp <= 5000) {
+    nvl = "Prata"
+
+} else if (XP >= 5001 && xp <= 7000) {
+    nvl = "Ouro"
+
+} else if (XP >= 7001 && xp <= 8000) {
+    nvl = "Platina"
+
+} else if (XP >= 8001 && xp <= 9000) {
+    nvl = "Ascendente"
+
+} else if (XP >= 9001 && xp <= 10000) {
+    nvl = "Imortal"
+
+} else {
+    nvl = "Radiante"
+
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nvl)   
